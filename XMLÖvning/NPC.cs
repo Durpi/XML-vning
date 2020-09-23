8d9439f2f48e475b80692ca28cde02e1b9157891
Pontus <02pontuskarlsson@gmail.com>
2020-09-23 10:55:12 +0200

    Huvud sakerna (klara)

---
+++ XMLÖvning/NPC.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace XMLÖvning
+{
+    public class NPC
+    {
+        
+        public string name = "";
+        public string age = "";
+        public string greeting = "";
+
+
+    }
+}
--- XMLÖvning/Program.cs
+++ XMLÖvning/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace XMLÖvning
 {
@@ -6,7 +9,33 @@ namespace XMLÖvning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            NPC villageMayor = new NPC();
+
+            Console.WriteLine("What is the NPC's name?");
+            villageMayor.name = Console.ReadLine();
+
+            Console.WriteLine("What is his/hers age?");
+            villageMayor.age = Console.ReadLine();
+
+            Console.WriteLine("What will he say when greeting you?");
+            villageMayor.greeting = Console.ReadLine();
+
+
+            XmlSerializer npcSerializer = new XmlSerializer(typeof(NPC));
+
+            using (FileStream file = File.Open(@"npc.xml", FileMode.OpenOrCreate))
+
+            {
+
+            npcSerializer.Serialize(file, villageMayor);
+
+            }
+
+            
+
+
+
         }
     }
 }
